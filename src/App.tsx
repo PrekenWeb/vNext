ff7d74e6248ec2bc293bc804e7309cd86a65ccc3
Stef Bakker <stef@buro26.nl>
2022-02-28 12:18:50 +0100

    Rewrite js code to ts

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+import './App.scss';
+import Header from './components/header/header';
+
+const App: FC = () => {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App; 
---
+++ src/components/header/header.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+
+
+const Header: FC = () => {
+  return (
+    <>
+      <h1>Prekenweb</h1>
+    </>
+  );
+};
+
+export default Header;
